b8cb01065a4e6917695629763bc8559fda0e1b0c
Rafael Souza <rafael_popo@hotmail.com>
2020-10-07 18:44:51 -0300

    Aula-02 - Heranca

--- JavaScript_Interfaces-Herança-em-OO/Conta.js
+++ JavaScript_Interfaces-Herança-em-OO/Conta.js
@@ -5,26 +5,44 @@ export class Conta {
         this._agencia = agencia;
     }
 
-    sacar(valor){
-        taxa = 1.1 * valor;
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
         }
     }
 
-    depositar(valor){
-        if(valor <= 0)
-        {
-            return;
-        } 
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }    
+
+    sacar(valor){
+        let taxa = 1;
+        return this._sacar(valor, taxa);
+    }
+
+    _sacar(valor, taxa){
+        const valorSacado = taxa * valor;
+        if(this._saldo >= valorSacado){
+            this._saldo -= valorSacado;
+            return valorSacado;
+        }
+
+        return 0;
+    }
+
+    depositar(valor){ 
         this._saldo += valor;           
     }
 
     tranferir(valor, conta){
-        
+
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
         
-    }    
+    }   
+
 }
--- JavaScript_Interfaces-Herança-em-OO/README.md
+++ JavaScript_Interfaces-Herança-em-OO/README.md
@@ -1,6 +1,6 @@
 ### Sumário
 
-**Conhecendo o problema do cliente.**
+**AULA-01 - Conhecendo o problema do cliente.**
 
 - Relembramos conceitos de classe, Propriedades,
 - Boas práticas na hora de declarar atributos
@@ -8,3 +8,13 @@
 - Problemas de duplicação de código
 - Problemas de código extremamente compartilhado
 
+**AULA-02 - Herança**
+
+- Especializando código
+- Herdando propriedades e atributos
+- Sobrecarga de métodos
+- Compartilhando código de forma segura
+- Mantendo nosso código na linguagem do cliente
+- Evitando código muito complicado através da herança
+- Palavra-chave Super
+- Método e propriedades privadas e protegidas
