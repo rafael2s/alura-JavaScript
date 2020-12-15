f16dead3fcb06188dc6f2f94566019ec7b98cad0
rafael2s <rafael_popo@hotmail.com>
2020-12-15 16:14:07 -0300

    aula 03

--- JavaScript_Salvando-dados-local-IndexedDB/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JavaScript_Salvando-dados-local-IndexedDB/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -15,15 +15,42 @@ class NegociacaoController {
        
         this._mensagem = new Bind(
             new Mensagem(), new MensagemView($('#mensagemView')),
-            'texto');       
+            'texto');
+            
+        this._ordemAtual = '';
+
+        ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.listaTodos())
+            .then(negociacoes => 
+                negociacoes.forEach(negociacao =>
+                    this._listaNegociacoes.adiciona(negociacao)))
+            .catch(erro => {
+                console.log(erro);
+                this._mensagem.texte = error;
+            })
     }
     
     adiciona(event) {
-        
+
         event.preventDefault();
-        this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._mensagem.texto = 'Negociação adicionada com sucesso'; 
-        this._limpaFormulario();   
+
+        ConnectionFactory
+            .getConnection()
+            .then(connection => {
+
+                let negociacao = this._criaNegociacao();
+                new NegociacaoDao(connection)
+                    .adiciona(negociacao)
+                    .then(() => {
+                        this._listaNegociacoes.adiciona(negociacao);    
+                        this._mensagem.texto = 'Negociação adicionada com sucesso'; 
+                        this._limpaFormulario();        
+                    })
+            })
+            .catch(erro => this._mensagem.texto = erro);
+           
     }
     
     importaNegociacoes() {
@@ -41,16 +68,22 @@ class NegociacaoController {
     
     apaga() {
         
-        this._listaNegociacoes.esvazia();
-        this._mensagem.texto = 'Negociações apagadas com sucesso';
+        ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.apagaTodos())
+            .then(mensagem => {
+                this._mensagem.texto = mensagem;
+                this._listaNegociacoes.esvazia();
+            });        
     }
     
     _criaNegociacao() {
         
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
-            this._inputQuantidade.value,
-            this._inputValor.value);    
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value));    
     }
     
     _limpaFormulario() {
